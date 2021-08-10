fae1479178236cb3a527d4db450f0a5e9927530f
Suraj Ajay Dwivedi <66689016+Surajajaydwivedi@users.noreply.github.com>
2021-08-11 02:24:32 +0530

    User Side Update

---
+++ src/data.js
@@ -0,0 +1,29 @@
+const data = {
+    
+    items: [
+        {
+            name : "Aloo Paratha",
+            price: "40",
+            available: "true"
+        },
+        {
+            name : "Gobi Paratha",
+            price: "60",
+            available: "false"
+        },{
+            name : "Chole Bhatue",
+            price: "100",
+            available: "true"
+        },
+        {
+            name : "Chole Kulche",
+            price: "60",
+            available: "false"
+        }
+    ]
+}
+
+export default data;
+
+
+
