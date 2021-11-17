9cae81a5d00e795b2833e194ede859e3ecbaef24
Aryaman <kumararyaman26@gmail.com>
2021-11-17 16:41:01 +0530

    17-11-2021

--- Client/src/components/InfoSection/SearchBar.js
+++ Client/src/components/InfoSection/SearchBar.js
@@ -1,10 +1,22 @@
 import React, { useState } from "react";
 import "./SearchBar.css";
-import SearchIcon from "@material-ui/icons/Search";
-import CloseIcon from "@material-ui/icons/Close";
+import { makeStyles, withStyles } from '@material-ui/core/styles';
+import FormControl from '@material-ui/core/FormControl';
+import NativeSelect from '@material-ui/core/NativeSelect';
+
+const useStyles = makeStyles((theme) => ({
+  margin: {
+    margin: theme.spacing(1),
+  },
+}));
 
 function SearchBar({ placeholder, data }) {
   const [filteredData, setFilteredData] = useState([]);
+  const classes = useStyles();
+  const [city, setCity] = React.useState('');
+  const handleChange = (event) => {
+    setCity(event.target.value);
+  }
   const [wordEntered, setWordEntered] = useState("");
 
   const handleFilter = (event) => {
@@ -21,11 +33,6 @@ function SearchBar({ placeholder, data }) {
     }
   };
 
-  const clearInput = () => {
-    setFilteredData([]);
-    setWordEntered("");
-  };
-
   return (
     <div className="search">
       <div className="searchInputs">
@@ -35,14 +42,22 @@ function SearchBar({ placeholder, data }) {
           value={wordEntered}
           onChange={handleFilter}
         />
-        <div className="searchIcon">
-          {filteredData.length === 0 ? (
-            <SearchIcon />
-          ) : (
-            <CloseIcon id="clearBtn" onClick={clearInput} />
-          )}
-        </div>
       </div>
+      <FormControl className={classes.margin}> 
+      <div className="SelectCity">
+        <NativeSelect
+          defaultValue={1}
+          id="City"
+          onChange={handleChange}   
+        >
+          <option aria-label="None" value="" />
+          <option value={1}> City</option>
+          <option value={2}>Delhi</option>
+          <option value={3}>Noida</option>
+          <option value={4}>Hyderabad</option>
+        </NativeSelect>
+        </div>
+      </FormControl>
       {filteredData.length != 0 && (
         <div className="dataResult">
           {filteredData.slice(0, 15).map((value, key) => {
