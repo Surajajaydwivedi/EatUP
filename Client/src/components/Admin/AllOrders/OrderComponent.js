cb7a9d4cec560e094606409bfbc3cf7a7d0f400a
Divyansh <Divyanshsikarwar@gmail.com>
2021-11-23 02:31:43 +0530

    23/11/21

--- Client/src/components/Admin/AllOrders/OrderComponent.js
+++ Client/src/components/Admin/AllOrders/OrderComponent.js
@@ -53,20 +53,7 @@ const useStyles = makeStyles((theme) => ({
     marginLeft: "-10px",
   },
 }));
-function parseTime(s) {
-  var c = s.split(":");
-  return parseInt(c[0]) * 60 + parseInt(c[1]);
-}
-function formatAMPM(date) {
-  var hours = date.getHours();
-  var minutes = date.getMinutes();
-  var ampm = hours >= 12 ? "PM" : "AM";
-  hours = hours % 12;
-  hours = hours ? hours : 12; // the hour '0' should be '12'
-  minutes = minutes < 10 ? "0" + minutes : minutes;
-  var strTime = hours + ":" + minutes + " " + ampm;
-  return strTime;
-}
+
 
 export default function RecipeReviewCard(orderdetails) {
   const classes = useStyles();
